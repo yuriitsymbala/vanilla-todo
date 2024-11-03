acf6f9a33ce37c420d9109d86aa10a4ec05634a5
Yurii Tsymbala <yurii.tsymbala@devcom.com.ua>
2024-11-03 18:02:59 +0200

    Refactored todo-list-container

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-/* Add ui logic to show/hide add-todo-container 
-    - add-todo-create-btn clicked - show separator + form + btn wrapper + hide self
-    - add-todo-save-btn   clicked - all hiden, only show add-todo-create-btn  
-    - add-todo-cancel-btn clicked - same logic as save
-*/
+/* show/hide add-todo-container */
 
 const createBtn = document.getElementById('add-todo-create-btn');
 const saveBtn = document.getElementById('add-todo-save-btn');
@@ -10,6 +6,7 @@ const cancelBtn = document.getElementById('add-todo-cancel-btn');
 const separator = document.getElementById('separator');
 const addTodoForm = document.getElementById('add-todo-form');
 const actionsBtnWrapper = document.getElementById('add-todo-actions-wrapper');
+const todoListContainer = document.getElementById('todo-list-container');
 
 createBtn.onclick = () => {
     separator.style.display = 'flex';
@@ -31,3 +28,29 @@ cancelBtn.onclick = () => {
     actionsBtnWrapper.style.display = 'none';
     createBtn.style.display = 'flex';
 };
+
+/* fetch todos */
+
+const todos = [
+    {
+        id: 0,
+        title: 'Купити продукти',
+        description: 'Скласти список необхідних продуктів та відвідати магазин.',
+        category: 'ДОМАШНІ СТРАВИ',
+        isDone: false
+    },
+    {
+        id: 1,
+        title: 'Пройти прогулянку на свіжому повітрі',
+        description: 'Встановити таймер на 30 хвилин і прогулятися в парку.',
+        category: 'ФІТНЕС',
+        isDone: true
+    }
+];
+
+for (const todo of todos) {
+    /* Todo cell configuration */
+    const todoCell = document.createElement('div');
+    todoCell.className = 'todo-item';
+    todoListContainer.appendChild(todoCell);
+}
